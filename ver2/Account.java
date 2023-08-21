e14667ec1f8be0859dd7f96ef99d6cf59948e983
TruEcho <142761298+EdgarJGamez@users.noreply.github.com>
2023-08-20 23:31:06 -0400

    Removed a constructor

--- ver2/Account.java
+++ ver2/Account.java
@@ -6,11 +6,6 @@ public class Account {
 	private String name;
 
 	// Constructor
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
-
-	// Constructor
 	public Account(String name, double balance) {
 		this.name = name;
 		this.balance = balance;
